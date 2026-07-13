afde8c6cb1d1b5c6c6a693578ac194c7db11ba6e
DanielS-source <danielsilvaiglesias@gmail.com>
2026-07-13 21:08:31 +0200

    Adjusted frame of the timestamp

--- src/components/MessageTimestamp/MessageTimestamp.tsx
+++ src/components/MessageTimestamp/MessageTimestamp.tsx
@@ -24,14 +24,14 @@ export default function MessageTimestamp({ timestamp, isMine }: MessageTimestamp
     <div className={styles.wrap}>
       <svg
         className={isMine ? styles.svgMirrored : undefined}
-        width="99"
+        width="101"
         height="45"
-        viewBox="0 0 99 45"
+        viewBox="0 0 101 45"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path d="M26 27L0 44.5L47.5 43L46.5 33L99 29L97 0L23 8L26 27Z" fill={outerColor} />
-        <path d="M30 29L9.5 41L43 39L42 29L95.5 25.5L89.5 4L29 10.5L30 29Z" fill={innerColor} />
+        <path d="M26 27.5L0 45L47.5 43.5L46.5 33.5L99 29.5L101 0L23 8.5L26 27.5Z" fill={outerColor} />
+        <path d="M30 29.5L9.5 41.5L43 39.5L42 29.5L95.5 26L92.5 4L29 11L30 29.5Z" fill={innerColor} />
       </svg>
       <span
         className={`${styles.time} ${isMine ? styles.timeMine : styles.timeThem}`}
